7f8e1c145ec4cb3b57792cfff4615aed30468e33
Marcel Bro <marcel.broz@gmail.com>
2022-12-03 02:11:44 +0000

    Gradle task added to generate input and source files easier

--- src/Utils.kt
+++ src/Utils.kt
@@ -21,18 +21,28 @@ abstract class Day(private val dayNumber: Int) {
     abstract fun part1(input: List<String>): Int
     abstract fun part2(input: List<String>): Int
 
-    fun run() {
+    fun run(expectedResultPart1: Int, expectedResultPart2: Int) {
         // test if implementation meets criteria from the description, like:
-        val testInput = readInput("Day${dayNumber}_test")
-        checkTest(part1(testInput), 24000)
-        checkTest(part2(testInput), 45000)
+        val dayString = getDayString(dayNumber)
+        val testInput = readInput("Day${dayString}_test")
+        checkTest(part1(testInput), expectedResultPart1)
+        checkTest(part2(testInput), expectedResultPart2)
 
-        val input = readInput("Day$dayNumber")
+        val input = readInput("Day$dayString")
+        println("=== DAY $dayNumber ===")
         println("Part 1 result: ${part1(input)}")
         println("Part 2 result: ${part2(input)}")
     }
 
     private fun checkTest(result: Int, expected: Int) {
         check(result == expected) { "Test check failed, wrong result: $result" }
+    }
+}
+
+fun getDayString(dayNumber: Int): String {
+    return if (dayNumber / 10 == 0) {
+        "0$dayNumber"
+    } else {
+        dayNumber.toString()
     }
 }
