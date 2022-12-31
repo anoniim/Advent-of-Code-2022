682fd0620d7a7efbf62e4cfe996f0928d2ec896e
Marcel Bro <marcel.broz@gmail.com>
2022-12-31 23:39:11 +0000

    Day 3 part 1 WIP

---
+++ src/Day03.kt
@@ -0,0 +1,58 @@
+fun main() {
+    Day3().run(
+        157,
+        -1
+    )
+}
+
+private class Day3 : Day(3) {
+
+    override fun part1(input: List<String>): Int {
+        // What is the sum of the priorities of those item types?
+        val sharedItemTypes = mutableSetOf<ItemType>()
+        var count = 0
+        input.forEach { rucksack ->
+            val compartments = getCompartments(rucksack)
+            sharedItemTypes.addAll(getSharedItemTypes(compartments))
+            count++
+        }
+        return sharedItemTypes.sumOf { getPriority(it) }
+            .also { println("Total of $count rucksacks (priority sum: $it)") }
+    }
+
+    private fun getPriority(itemType: ItemType): Int {
+        val priority = if (itemType.isLowerCase()) {
+            itemType.code - 96
+        } else {
+            itemType.code - 38
+        }
+        println("Priority of $itemType is $priority")
+        return priority
+    }
+
+    private fun getCompartments(rucksack: String): Compartments {
+        val middleIndex = rucksack.length / 2
+        val firstCompartment = rucksack.substring(0, middleIndex)
+        val secondCompartment = rucksack.substring(middleIndex, rucksack.length)
+        return Compartments(firstCompartment, secondCompartment)
+    }
+
+    private fun getSharedItemTypes(compartments: Compartments): Set<ItemType> {
+        return mutableSetOf<ItemType>().apply {
+            compartments.first.forEach {
+                if (compartments.second.contains(it)) {
+                    add(it)
+                }
+            }
+        }
+    }
+
+    override fun part2(input: List<String>): Int {
+        //
+        return -1
+    }
+
+}
+
+private typealias Compartments = Pair<String, String>
+private typealias ItemType = Char
