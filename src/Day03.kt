da444c3c8d9ea83ec6f2f6857691e09df649d9b4
Marcel Bro <marcel.broz@gmail.com>
2022-12-31 23:52:23 +0000

    Day 3 part 1 removed unused code and logging

--- src/Day03.kt
+++ src/Day03.kt
@@ -10,24 +10,19 @@ private class Day3 : Day(3) {
     override fun part1(input: List<String>): Int {
         // What is the sum of the priorities of those item types?
         val sharedItemTypes = mutableListOf<ItemType>()
-        var count = 0
         input.forEach { rucksack ->
             val compartments = getCompartments(rucksack)
             sharedItemTypes.addAll(getSharedItemTypes(compartments))
-            count++
         }
         return sharedItemTypes.sumOf { getPriority(it) }
-            .also { println("Total of $count rucksacks (priority sum: $it)") }
     }
 
     private fun getPriority(itemType: ItemType): Int {
-        val priority = if (itemType.isLowerCase()) {
+        return if (itemType.isLowerCase()) {
             itemType.code - 96
         } else {
             itemType.code - 38
         }
-        println("Priority of $itemType is $priority")
-        return priority
     }
 
     private fun getCompartments(rucksack: String): Compartments {
@@ -40,16 +35,6 @@ private class Day3 : Day(3) {
     private fun getSharedItemTypes(compartments: Compartments): Set<ItemType> {
         return compartments.first.toSet() intersect compartments.second.toSet()
     }
-
-//    private fun getSharedItemTypes(compartments: Compartments): Set<ItemType> {
-//        return mutableSetOf<ItemType>().apply {
-//            compartments.first.forEach {
-//                if (compartments.second.contains(it)) {
-//                    add(it)
-//                }
-//            }
-//        }
-//    }
 
     override fun part2(input: List<String>): Int {
         // Find the item type that corresponds to the badges of each three-Elf group.
